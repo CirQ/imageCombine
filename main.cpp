4296cb5a76c6e61e36d43da34f86e62e16ffbd96
CirQ <cirq999@163.com>
2017-10-08 00:13:05 +0800

    add method for combining two images via interpolation

--- main.cpp
+++ main.cpp
@@ -6,9 +6,12 @@
 using namespace cv;
 using namespace std;
 
-int loadImages(Mat& img1, Mat& img2, const char* name1, const char* name2){
-    int flag = 0;
+int loadImages(Mat &img1, Mat &img2, const char *name1, const char *name2){
+    /**************************************************************************
+     * Open two images with given file names, if sucess, returns 0, else 1.
+     **************************************************************************/
 
+    int flag = 0;
     img1 = imread(name1);
     if(!img1.data){
         cerr << "File " << name1 << " does not exit!" << endl;
@@ -19,44 +22,66 @@ int loadImages(Mat& img1, Mat& img2, const char* name1, const char* name2){
         cerr << "File " << name2 << " does not exit!" << endl;
         flag = -1;
     }
-
     return flag;
 }
 
-int main(int argc, char* argv[]) {
+int combineViaInterpolation(Mat &show, Mat &hide, Mat &result){
+    /**************************************************************************
+     * The show is when image displayed in small scale, while the hide is at
+     * another case. Due to this requirement, we assume this condition helds:
+     *                          show.size < hide.size
+     * in this implementation, show.size = hide.size / 2.
+     **************************************************************************/
+
+    int flag = 0;
+    try{
+        hide.copyTo(result);
+        Mat resizedShow;
+        resize(show, resizedShow, Size_<int>(hide.cols/2, hide.rows/2), 0, 0, INTER_NEAREST);
+        if(result.isContinuous()){
+            for(int h = 0; h < result.rows; h++){
+                if(h%2 != 1) // control which pixel to be overwritten
+                    continue;
+
+                auto *rowPtr = result.ptr<Vec3b>(h);
+                for(int w = 0; w < result.cols; w++){
+                    if(w%2 != 1) // control which pixel to be overwritten
+                        continue;
+
+                    rowPtr[w] = resizedShow.at<Vec3b>(h/2, w/2);
+                }
+            }
+        } else{
+            cerr << "error during image scanning (seems not your fault)" << endl;
+            flag = -1;
+        }
+    } catch(exception& e){
+        cerr << "error during interpolation: " << e.what() << endl;
+        flag = -1;
+    }
+    return flag;
+}
+
+void displayWithInterpolation(Mat& image){
+
+}
+
+int main(int argc, char *argv[]){
     if(argc != 3){
         cerr << "Usage: " << argv[0] << " <filename1> <filename2>" << endl;
+        cerr << "<filename1> stands for image which displayed in small scale, white background," << endl;
+        cerr << "<filename2> stands for image which displayed in large scale, black background." << endl;
         return EXIT_FAILURE;
     }
 
-    Mat showImage, hideImage;
-    int loadImageFlag = loadImages(showImage, hideImage, argv[1], argv[2]);
-    if(loadImageFlag == -1)
+    Mat showImage, hideImage, dstImage;
+    int loadImagesFlag = loadImages(showImage, hideImage, argv[1], argv[2]);
+    if(loadImagesFlag == -1)
         return EXIT_FAILURE;
 
-    //Vec3b intensity = hideImage.at<Vec3b>(1, 3);
-    //cout << intensity << endl;
-
-    //cout << hideImage.rows << hideImage.cols << endl;
-
-    //switch(hideImage.channels()){
-    //    case 1: {
-    //        break;
-    //    }
-    //    case 3: {
-    //        MatIterator_<Vec3b> it, end;
-    //        int i = 0;
-    //        for(it = hideImage.begin<Vec3b>(), end = hideImage.end<Vec3b>(); it != end; it++){
-    //            cout << i << *it << endl;
-    //            i++;
-    //        }
-    //        break;
-    //    }
-    //    default:
-    //        break;
-    //}
-
-
-
+    int combineImagesFlag = combineViaInterpolation(showImage, hideImage, dstImage);
+    if(combineImagesFlag == -1)
+        return EXIT_FAILURE;
+    displayWithInterpolation(dstImage);
     return EXIT_SUCCESS;
 }
